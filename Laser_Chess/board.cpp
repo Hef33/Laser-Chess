41a141aa61c74b573d52ecc05db5a09ca7aac130
Hef33 <krzysztofartiemjew@gmail.com>
2026-05-29 17:33:31 +0200

    Tworzenie planszy
    Pierwsze pliki projektu, stworzenie klasy Board. Obecnie plansza jest 8x8, ale może się to zmienić wraz z dalszym rozwojem projketu

---
+++ Laser_Chess/board.cpp
@@ -0,0 +1,32 @@
+#include "Board.h"
+#include "Global_Values.h"
+
+
+Board::Board() {
+    setupTiles();
+}
+
+void Board::setupTiles() {
+    for (int x = 0; x < 8; ++x) {
+        for (int y = 0; y < 8; ++y) {
+            tiles[x][y].setSize(sf::Vector2f(TILE_SIZE, TILE_SIZE));
+            tiles[x][y].setPosition(x * TILE_SIZE, y * TILE_SIZE);
+
+            // Szachownica: naprzemienne kolory
+            // Jeśli suma współrzędnych jest parzysta - pole jest jasne
+            if ((x + y) % 2 == 0) {
+                tiles[x][y].setFillColor(sf::Color(240, 217, 181)); // Jasne pole
+            } else {
+                tiles[x][y].setFillColor(sf::Color(181, 136, 99));  // Ciemne pole
+            }
+        }
+    }
+}
+
+void Board::draw(sf::RenderWindow& window) {
+    for (int x = 0; x < 8; ++x) {
+        for (int y = 0; y < 8; ++y) {
+            window.draw(tiles[x][y]);
+        }
+    }
+}
